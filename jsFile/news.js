88ac0c73764c4cd19b2ff514d48862f81d6f2975
Toufique18 <toufiqueeamam7@gmail.com>
2024-03-03 18:02:16 +0600

    lets discuss section active status  done and commit

--- jsFile/news.js
+++ jsFile/news.js
@@ -1,5 +1,5 @@
-const loadNews = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts')
+const loadNews = async (searchText = 'comedy') => {
+    const res = await fetch(` https://openapi.programming-hero.com/api/retro-forum/posts?category=${searchText}`)
     const data = await res.json();
     const news = data.posts;
     //console.log(news);
@@ -9,16 +9,24 @@ const loadNews = async () => {
 const displayNews = (news) => {
     //console.log(news);
     const newsContainer = document.getElementById('news-Container');
+    const indicatorChange = document.getElementById('indicator-change');
+
+    //newsContainer.textContent= "";
+    
+
 
     news.forEach(New => {
         console.log(New);
+        
+
+
 
         const newsCard = document.createElement('div');
         newsCard.classList = `flex gap-4 w-full bg-zinc-100 rounded-3xl p-5 mb-3 hover:bg-indigo-400 hover:bg-opacity-10 hover:border-indigo-400 hover:border`;
         newsCard.innerHTML = `
         <div>
                         <div class="indicator">
-                            <span class="indicator-item indicator-start sm:indicator-middle md:indicator-bottom indicator-top indicator-end xl:indicator-end badge badge-secondary"></span> 
+                            <span id="indicator-change" class="indicator-item indicator-start md:indicator-bottom border-none indicator-top indicator-end xl:indicator-end badge badge-secondary"></span> 
                             <div class="grid w-16 h-16 bg-base-300 place-items-center rounded-xl"><img class= "rounded-xl" src="${New.image}" alt=""></div>
                           </div>
                     </div>
@@ -47,9 +55,40 @@ const displayNews = (news) => {
 
                     </div>
         `
-        newsContainer.appendChild(newsCard);
+        
 
+        newsContainer.appendChild(newsCard);
+        //activeStatus(New.isActive);
+        const indicatorChange = newsCard.querySelector('.indicator-item'); // Select indicator within the scope of the news card
+        if (New.isActive) {
+            indicatorChange.classList.add('bg-green-500');
+        }else{
+            indicatorChange.classList.add('bg-red-500');
+        }
     })
+    
 }
+
+//handle search
+const handleSearch = () =>{
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    console.log(searchText)
+    loadNews(searchText);
+}
+
+
+// const activeStatus = (isActiving) => {
+//     const indicatorChange = document.getElementById('indicator-change')
+//     if (isActiving) {
+//         indicatorChange.classList.add('bg-green-500');
+//         indicatorChange.classList.remove('bg-red-500');
+
+//     }
+//     else {
+//         indicatorChange.classList.add('bg-red-500');
+//         indicatorChange.classList.remove('bg-green-500');
+//     }
+// }
 
 loadNews();
